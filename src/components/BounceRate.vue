22b5d94ea1e0716a211e45bedb4293732f551144
vishnu <t.vishnu@voltuswave.com>
2024-07-18 14:26:20 +0530

    first commit

--- src/components/BounceRate.vue
+++ src/components/BounceRate.vue
@@ -18,9 +18,7 @@
 </template>
 
 <script>
-// import { Line } from 'vue-chartjs'
-// import { Chart as ChartJS, Title, Tooltip, Legend, LineElement, CategoryScale, LinearScale, PointElement } from 'chart.js'
-// ChartJS.register(Title, Tooltip, Legend, LineElement, CategoryScale, LinearScale, PointElement)
+
 
 export default {
   name: 'BounceRate',
